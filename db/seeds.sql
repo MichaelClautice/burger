3dd9a096d1c8ff9724e879a6b8d7498e8fca885c
Michael Clautice <michaelclauticeusa@gmail.com>
2018-03-06 13:22:29 -0500

    7th commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES
+  ("Black and Blue Burger", false, ""),
+  ("Da Banger Burger", false, ""),
+  ("The Phoenix Burger", false, ""),
+  ("Gringo Burger", false, ""),
+  ("Cajun Burger", false, "");
