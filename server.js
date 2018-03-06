cf156d2226a73f0ef84fa0b16a222fa6da308d0a
Michael Clautice <michaelclauticeusa@gmail.com>
2018-03-06 12:15:19 -0500

    5th commit

---
+++ server.js
@@ -0,0 +1,8 @@
+
+// require pkgs aka dependencies
+// Initializes the npm packages used
+const express = require("express");
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+const app = express();
